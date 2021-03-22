c86c562213e5331f96cf4f006e984413d03082d9
dyan127 <80673667+dyan127@users.noreply.github.com>
2021-03-22 10:08:29 +0700

    Tugas minggu 7

---
+++ Minggu7/application/views/demoview.php
@@ -0,0 +1,21 @@
+<html> 
+<head>
+<title> Demo view CSS</title>
+<!--<style type = " text/css">
+    h2{
+        color : #f00;
+        font-size : 20px;
+        border-buttom : dashed 1px #f00;
+    }
+    p{
+        font-style : italic ;
+    }
+    
+</style>-->
+<link rel=" stylesheet" type="text/css" href="<?php //echo base_url(); ?>aseets/css/style.css">
+</head>
+<body>
+    <h2> Demo View dengan CSS</h2>
+    <p> contoh demo dengan css</p>
+</body>
+</html>
